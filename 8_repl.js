490baaec821e3bdb707e588782a6b1886e56f1de
rodrigo debian <correo.rodrigo@gmail.com>
2021-01-13 09:21:00 -0600

    repl

--- 8_repl.js
+++ 8_repl.js
@@ -1,12 +1,4 @@
 // https://otcatchup.util.repl.co/
-/*
-'[{"op": "skip", "count": 40}, {"op": "delete", "count": 47}]'
-'[{"op": "skip", "count": 45}, {"op": "delete", "count": 47}]'
-'[{"op": "skip", "count": 40}, {"op": "delete", "count": 47}, {"op": "skip", "count": 2}]'
-'[{"op": "delete", "count": 7}, {"op": "insert", "chars": "We"}, {"op": "skip", "count": 4}, {"op": "delete", "count": 1}]'
-'[]'
-
-*/
 
 let isValid = (stale, latest, otjson) => {
     let options = JSON.parse(otjson);
@@ -14,61 +6,57 @@ let isValid = (stale, latest, otjson) => {
     let max = stale.length;
     let result = [];
 
-    for(let it of options) {
-        switch(it.op) {
+    for (let it of options) {
+        switch (it.op) {
             case "skip":
-                result.push(stale.slice(i, it.count));
+                result.push(stale.slice(i, i + it.count));
                 i += it.count;
-                if (i > max)  {
-                    console.log("false, skip past end");
-                    return false;
-                }
                 break;
             case "delete":
                 i += it.count;
-                if (i > max)  {
-                    console.log("false, delete past end");
-                    return false;
-                }
                 break;
             case "insert":
-                result.push([...it.chars]);
+                result.push(it.chars);
                 break;
         }
-    }
 
-    console.assert(latest === result.join(), result.join());
-    console.log("true");
+        if (i > max) {
+            return false;
+        }
+
+    }
+    let actual = result.join("") + stale.slice(i);
+    console.assert(latest === actual, actual);
+
     return true;
 }
 
-isValid(
+console.assert(isValid(
     'Repl.it uses operational transformations to keep everyone in a multiplayer repl in sync.',
     'Repl.it uses operational transformations.',
     '[{"op": "skip", "count": 40}, {"op": "delete", "count": 47}]'
-  ); // true
+), "it should be true"); // true
 
-  
-  isValid(
+console.assert(isValid(
     'Repl.it uses operational transformations to keep everyone in a multiplayer repl in sync.',
     'Repl.it uses operational transformations.',
     '[{"op": "skip", "count": 45}, {"op": "delete", "count": 47}]'
-  ); // false, delete past end
-  
-  isValid(
+) === false, "it should be false"); // false, delete past end
+
+console.assert(isValid(
     'Repl.it uses operational transformations to keep everyone in a multiplayer repl in sync.',
     'Repl.it uses operational transformations.',
     '[{"op": "skip", "count": 40}, {"op": "delete", "count": 47}, {"op": "skip", "count": 2}]'
-  ); // false, skip past end
-  
-  isValid(
+) === false, "it should be false"); // false, skip past end
+
+console.assert(isValid(
     'Repl.it uses operational transformations to keep everyone in a multiplayer repl in sync.',
     'We use operational transformations to keep everyone in a multiplayer repl in sync.',
     '[{"op": "delete", "count": 7}, {"op": "insert", "chars": "We"}, {"op": "skip", "count": 4}, {"op": "delete", "count": 1}]'
-  ); // true
-  
-  isValid(
+), "it should be true"); // true
+
+console.assert(isValid(
     'Repl.it uses operational transformations to keep everyone in a multiplayer repl in sync.',
     'Repl.it uses operational transformations to keep everyone in a multiplayer repl in sync.',
     '[]'
-  ); // true
+), "it should be true"); // true
